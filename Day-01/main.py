6032c2fd553244b2994168722cbddc9fb84f19bf
rajat-singh <rajatsingh11385@gmail.com>
2026-06-02 12:31:05 +0530

    Built a simple calculator with zero division error handling
    Learn data types , input and built calculator for + , - , * , /

--- Day-01/main.py
+++ Day-01/main.py
@@ -19,3 +19,50 @@ print("Hii\tSir")
 print("Hii\n Sir")
 #Separator cgaracter
 print("Hey",6,7 ,sep = "~")
+#Data types and variables
+a =1 
+b = "Harry brokes"
+c = True
+d = None
+e = 5.6
+f = complex(4,6)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(f) 
+#operators
+#Arithmetic operators
+x = 2
+y = 4
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)#It will make disapper 0.5
+print(x ** y)
+print(x % y)  
+#End of day one with one exercise question...
+#Create a calculator
+m = int(input("Number 1:"))
+n = int(input("Number 2:"))
+print("Calculating all the values----")
+print("Addition", m + n)
+print("Subtraction", m - n)
+print("Multiplication", m * n)
+print("Divide", m / n)
+#It may be updated further bec one error is created if user entered n = 0
+#Again creating a calculator:
+p = int(input("No .1:"))
+q = int(input("No .2:"))
+print("Our values are being calculated")
+print("Addition:", p + q)
+print("Subtraction:", p - q)
+print("Multiplication:", p * q)
+if q == 0:
+    print("Cannot be divided by 0")
+else:
+    print("Divide:", p / q)
+ 
