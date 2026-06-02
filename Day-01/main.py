30253c710760049ee70e4b8476378f3e044b8cfe
rajat-singh <rajatsingh11385@gmail.com>
2026-06-02 07:22:41 +0530

    Learning Escape sequence character

--- Day-01/main.py
+++ Day-01/main.py
@@ -3,3 +3,19 @@ print("Hello world")
 print("Rajat Singh")  
 import pandas as pd
 print("Pandas version :",pd.__version__)
+#My first program
+name = "Rajat singh gaur"
+age = 17
+college = "Maharishi university of information technology"
+print("Hello",name,age,college)
+#Learning back slash n
+print("Hey I am a good boy\nand this viewer is also a good boy/girl")
+#Multi line comment
+'''
+Hii Rajat,What are you doing?
+'''
+#Escape sequence character
+print("Hii\tSir")
+print("Hii\n Sir")
+#Separator cgaracter
+print("Hey",6,7 ,sep = "~")
