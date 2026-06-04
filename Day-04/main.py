260b90b41f6577266e41a0f14659ffcc218b6473
rajat-singh <rajatsingh11385@gmail.com>
2026-06-04 07:00:24 +0530

    Basic Greeting Project

---
+++ Day-04/main.py
@@ -0,0 +1,13 @@
+#Basic time greeting mini-project:
+time = int(input("Enter hour in 24 hour format, ex-0-23:"))
+if(time >= 4 and time < 12):
+    print("Good Morning 🌄")
+elif(time >= 12 and time < 17):
+    print("Good Afternoon 🌅")
+elif(time >= 17 and time < 20):
+    print("Good evening 🌆")
+else:
+    print("good night 🌃")
+
+
+#Level up
