2b940528abec4a016ba00f57a60954eae26acba9
rajat-singh <rajatsingh11385@gmail.com>
2026-06-07 06:58:09 +0530

    Day-06: Learning Function And arguments
    -Defined first function calculate_sum(a ,b) to add two numbers.
    -Created IsGreater (a ,b) function with if-else for comparison.
    -Practiced function calls with different arguments: calculate_sum (7,6)
    -Explored function arguemnts and started default arguments.

--- Day-06/main.py
+++ Day-06/main.py
@@ -22,5 +22,28 @@ for i in range(1,6):
         break
     print(i,end = " ")
  
+#Function 
+def calculate_sum(a,b):
+    sum = a + b
+    print(sum)
 
-
+calculate_sum(6,7)
+calculate_sum(9,34)
+def IsGreater(a,b):
+    if(a > b):
+        print("First No is Greater")
+    else:
+        print("Second No is greater")
+IsGreater(9,8)
+IsGreater(3,7)
+#Function Arguments 
+#Default argument
+def Average(a = 24, b  = 24):
+    print("The average of two number:",(a + b)/2)
+    return
+Average() 
+#Required Argumnets
+def average(a , b ,c = 5):
+    print("The Average of  three numbers is:", (a + b + c)/3)
+average(5,5)#C Is optional 
+ 
