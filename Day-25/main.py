13291db15e52c0a47708b5c0a1a7bb6cbdb1746a
rajat-singh <rajatsingh11385@gmail.com>
2026-07-13 11:18:54 +0530

    Day-25: SHUTIL MODULE

---
+++ Day-25/main.py
@@ -0,0 +1,10 @@
+# Shutil Module:
+import shutil
+# THIS METHOD IS USED TO COPY FILE.
+# shutil.copy("Day-19/main.py" , "Day-25/rajat.py")  
+# THIS METHOD IS USED TO COPY FOLDER.
+# shutil.copytree("Day-18" ,"My Day-18")
+# THIS METHOD IS USED TO MOVE ANY FILE.
+# shutil.move("Day-20/hackerrank_solution","hacker_solutions")
+# THIS METHOD TO USED TO DELETE ANY FOLDER. 
+# shutil.rmtree("My Day-18")
